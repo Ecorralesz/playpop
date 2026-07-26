7cd71cdf332908562515c6b748dd5439ad2619cb
Ecorralesz <82177112+Ecorralesz@users.noreply.github.com>
2026-07-26 12:32:19 -0600

    style: establish PlayPop design system foundation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import './styles/tokens.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/bootstrap-overrides.css';
 import './styles/global.css';
 import App from './App.jsx';
 
